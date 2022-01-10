24bdfb54a8cc831ecf6625f56479ce0f3f0dd9ba
blueprint-fred <frederick.g@blueprintbusinesssolutionscorp.com>
2022-01-11 01:05:50 +0800

    design auth init

--- src/auth/protected-route.js
+++ src/auth/protected-route.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import Form from '../components/Form'
 import { useStore } from '../context'
+import axios from 'axios'
 
 const ProtectedRoute = ({children}) => {
     const { 
         isLoggedIn, 
         setIsLoggedIn
-    } = useStore()
+    } = useStore();
+
+    const FetchData = useCallback(() => {
+        if(typeof window !== 'undefined' && localStorage.getItem('token')){
+            axios.get('/custom-auth/verify',{
+                headers: {
+                    Authorization:
+                    `Bearer ${localStorage.getItem('token')}`,
+                },
+            })
+            .then(({data}) => {
+                setTimeout(() => {
+                    setIsLoggedIn(true)
+                }, 2000);
+            })
+            .catch(err => {
+                console.log(err)
+                // logout()
+            })
+        }
+        else{
+            // logout()
+        }
+        // eslint-disable-next-line
+    }, [])
+
+    useEffect(() => {
+        FetchData()
+    }, [FetchData])
 
     return (
         <>
