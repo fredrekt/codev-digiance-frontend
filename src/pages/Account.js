3307ea0041fc5680bd04987bb882bf58f62e461d
blueprint-fred <frederick.g@blueprintbusinesssolutionscorp.com>
2022-01-11 01:07:24 +0800

    used logout func

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useStore } from '../context'
 
 const AccountPage = () => {
+    const { 
+        logout
+    } = useStore();
+    
     return (
         <div>
             <h1 className="black-text">Account Page</h1>
+            <button onClick={logout}>logout</button>
         </div>
     )
 }
