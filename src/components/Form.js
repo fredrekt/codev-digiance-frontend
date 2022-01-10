c0dd8e9ace1a1dab8f460043acba5513ad5fe28d
blueprint-fred <frederick.g@blueprintbusinesssolutionscorp.com>
2022-01-11 00:03:12 +0800

    init

---
+++ src/components/Form.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const {
+        email,
+        password
+    } = formData
+
+    const onChange = e => setFormData({ [e.target.name]: e.target.value });
+    const onSubmit = e => {
+        e.preventDefault();
+        console.log(JSON.stringify(formData))
+    }
+
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <div classname="m-auto">
+                <h3 className="black-text text-center font-bold text-6xl sm:text-7xl leading-tight mb-3">Codev</h3>
+                <form onSubmit={e=>onSubmit(e)} className="flex-col bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <input name="email" value={email} onChange={e=>onChange(e)} type="email" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 my-2 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Email"/>
+                        <input name="password" value={password} onChange={e=>onChange(e)} type="password" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 my-2 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Password"/>
+                    </div>
+                    <div className="flex-column items-center justify-between">
+                        <button type="submit" className="py-2 px-4 bg-black hover:bg-gray-50 text-white hover:text-black w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md">
+                            Sign In
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Form
