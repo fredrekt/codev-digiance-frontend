338ab9d2d2c54f405e084291bd33bed0da3541c3
blueprint-fred <frederick.g@blueprintbusinesssolutionscorp.com>
2022-01-11 08:44:47 +0800

    added page change-password

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {Routes as Switch, Route } from 'react-router-dom';
 import AccountPage from './pages/Account';
 import ProtectedRoute from './auth/protected-route';
+import ChangePassword from './pages/ChangePassword';
 
 function App() {
   return (
     <ProtectedRoute>
       <Switch>
         <Route exact path='/' element={<AccountPage/>}/>
+        <Route path="/change-password" element={<ChangePassword/>}/>
       </Switch>
     </ProtectedRoute>
   );
